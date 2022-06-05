466fa1a90091fa6d12af5382623c19bc6d99fd3d
Kenan Ege <61656752+KenanAegean@users.noreply.github.com>
2022-06-05 12:54:25 +0200

    nerdeyse finished

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ cards[4] = "cards/nature-shields-its-own.jpg";
 cards[5] = "cards/nothing-can-stop-me-now.jpg";
 cards[6] = "cards/the-very-soil-shall-shake.jpg";
 cards[7] = "cards/your-inescapable-doom.jpg";
-for (let index = 0; index <= 7; index++) {
+for (let index = 0; index < 8; index++) {
     ongoing[index] = cards[index];
 }
 cards[8] = "cards/a-display-of-my-dark-power.jpg";
@@ -67,7 +67,7 @@ AssembletheDoomsdayMachine[3] = cards[21];
 AssembletheDoomsdayMachine[4] = cards[21];
 AssembletheDoomsdayMachine[5] = cards[23];
 AssembletheDoomsdayMachine[6] = cards[1];
-AssembletheDoomsdayMachine[7] = cards[1];
+AssembletheDoomsdayMachine[7] = cards[7];
 AssembletheDoomsdayMachine[8] = cards[24];
 AssembletheDoomsdayMachine[9] = cards[24];
 AssembletheDoomsdayMachine[10] = cards[26];
@@ -148,21 +148,63 @@ TrampleCivilizationUnderfoot[17] = cards[49];
 TrampleCivilizationUnderfoot[18] = cards[19];
 TrampleCivilizationUnderfoot[19] = cards[19];
 
+var kenanDeck = new Array();
+kenanDeck[0] = cards[18];
+kenanDeck[1] = cards[18];
+kenanDeck[2] = cards[21];
+kenanDeck[3] = cards[22];
+kenanDeck[4] = cards[44];
+kenanDeck[5] = cards[49];
+kenanDeck[6] = cards[2];
+kenanDeck[7] = cards[29];
+kenanDeck[8] = cards[34];
+kenanDeck[9] = cards[34];
+kenanDeck[10] = cards[6];
+kenanDeck[11] = cards[4];
+kenanDeck[12] = cards[38];
+kenanDeck[13] = cards[39];
+kenanDeck[14] = cards[39];
+kenanDeck[15] = cards[9];
+kenanDeck[16] = cards[48];
+kenanDeck[17] = cards[45];
+kenanDeck[18] = cards[19];
+kenanDeck[19] = cards[8];
 
+var randomDeck = new Array();
+
+for (var i = 0; i <  3; i++) 
+{
+    var random = Math.floor(Math.random() * 8);
+    randomDeck[i] = ongoing[random];
+}
+for (var i = 3; i <  20; i++) 
+{
+    var random = Math.floor(Math.random() * (12) + 8);
+    randomDeck[i] = cards[random];
+}
+    
+
+//var i = 0;
 function deckCall(value)
 {
     checker(value);
     shuffleDeck(selectedArray);
-    ongoingFind(selectedArray, i);
-    document.getElementById("img1").src = selectedArray[0];
-    document.getElementById("img3").src = selectedArray[0];
+    ongoingFind(selectedArray, 0);
+    document.getElementById("smlPic").src = selectedArray[0];
+    document.getElementById("bigPic").src = selectedArray[0];
     count = 1;
     countShow.innerText = "Turn: " + 1;
+    document.getElementById("img1").src = "";
     document.getElementById("img2").src = "";
-    document.getElementById("img4").src = "";
-    document.getElementById("img5").src = "";
+    document.getElementById("img3").src = "";
+    for (var i = 0; i < 3; i++) 
+    {
+        ongoingTemp[i] = "";
+    }
 
 }
+
+
 
 
 function shuffleDeck(array) 
@@ -183,10 +225,10 @@ function imageClicked()
 {
     //alert(i);
     TurnCount();
-    if(i <= selectedArray.length - 1)
+    if(i < selectedArray.length )
     {
-        document.getElementById("img1").src = selectedArray[i];
-        document.getElementById("img3").src = selectedArray[i];
+        document.getElementById("smlPic").src = selectedArray[i];
+        document.getElementById("bigPic").src = selectedArray[i];
         ongoingFind(selectedArray, i);       
         i++;
                 
@@ -205,8 +247,8 @@ function imageClicked()
             i = 0;
         }
                
-        document.getElementById("img1").src = selectedArray[i];
-        document.getElementById("img3").src = selectedArray[i];
+        document.getElementById("smlPic").src = selectedArray[i];
+        document.getElementById("bigPic").src = selectedArray[i];
         ongoingFind(selectedArray, i);
         
     }    
@@ -220,67 +262,59 @@ function ongoingFind(array, i)
 
         if(ongoungCounter == 0)
         {
-            document.getElementById("img2").src = array[i];
+            document.getElementById("img1").src = array[i];
             ongoingTemp[0] = array[i];
             array.splice(i, 1);
             ongoungCounter++;
-            //alert("1 de çıkıyom");
         }
         else if(ongoungCounter == 1)
         {
-            document.getElementById("img4").src = array[i];
+            document.getElementById("img2").src = array[i];
             ongoingTemp[1] = array[i];
             array.splice(i, 1);
             ongoungCounter++;
-            //alert("aradığın" + i);
-            //alert("2 de çıkıyom");
         }
         else if(ongoungCounter == 2)
         {
-            //alert(i);
-            document.getElementById("img5").src = array[i];
+            document.getElementById("img3").src = array[i];
             ongoingTemp[2] = array[i];
             array.splice(i, 1);
-            ongoungCounter = 0;
-            //round = round*(-1);
-            //alert("3 de çıkıyom");
-        }
-        //alert("hop ordayım hop burda");
-                   
+            ongoungCounter++;
+        }                   
     }
 }
 
-var ogimgCounter = 2;
-function ongoingImageClicked()
+
+function mouseOver(id)
+{
+    document.getElementById("bigPic").src = document.getElementById(id).src;
+}
+
+//var ogimgCounter = 2;
+function mouseDown()
 {
     
-    if (ogimgCounter == 2)
+    if (ongoungCounter == 3)
     {
-        document.getElementById("img5").src = "";
-        ogimgCounter--;
-        selectedArray.push(ongoingTemp[0]);
-        qChecker = true;
-        //alert(selectedArray.length);
+        document.getElementById("img3").src = "";
+        ongoungCounter--;
+        selectedArray.push(ongoingTemp[2]);
     }
-    else if (ogimgCounter == 1)
-    {
-        
-        document.getElementById("img4").src = "";
-        ogimgCounter--;
-        selectedArray.push(ongoingTemp[1]);
-        qChecker = true;
-        //alert(selectedArray.length);
-    }
-    else if (ogimgCounter == 0)
+    else if (ongoungCounter == 2)
     {
         
         document.getElementById("img2").src = "";
-        ogimgCounter = 2;
-        selectedArray.push(ongoingTemp[2]);
-        qChecker = true;
+        ongoungCounter--;
+        selectedArray.push(ongoingTemp[1]);
+    }
+    else if (ongoungCounter == 1)
+    {
+        
+        document.getElementById("img1").src = "";
+        ongoungCounter--;
+        selectedArray.push(ongoingTemp[0]);
         round = round*(-1);
 
-        //alert(selectedArray.length);
         
     }
         
@@ -299,42 +333,42 @@ function checker(value)
     {
         for (let index = 0; index < 20; index++) 
         {
-            selectedArray[index] = AssembletheDoomsdayMachine[index];
+            selectedArray[index] = randomDeck[index];
         }
     }
     else if(value == 2)
     {
         for (let index = 0; index < 20; index++) 
         {
-            selectedArray[index] = ScorchtheWorldwithDragonfire[index];
+            selectedArray[index] = AssembletheDoomsdayMachine[index];
         }
     }
     else if(value == 3)
     {
         for (let index = 0; index < 20; index++) 
         {
-            selectedArray[index] = AssembletheDoomsdayMachine[index];
+            selectedArray[index] = BringAbouttheUndeadApocalypse[index];
         }
     }
     else if(value == 4)
     {
         for (let index = 0; index < 20; index++) 
         {
-            selectedArray[index] = AssembletheDoomsdayMachine[index];
+            selectedArray[index] = ScorchtheWorldwithDragonfire[index];
         }
     }
     else if(value == 5)
     {
         for (let index = 0; index < 20; index++) 
         {
-            selectedArray[index] = ScorchtheWorldwithDragonfire[index];
+            selectedArray[index] = TrampleCivilizationUnderfoot[index];
         }
     }
     else if(value == 6)
     {
         for (let index = 0; index < 20; index++) 
         {
-            selectedArray[index] = TrampleCivilizationUnderfoot[index];
+            selectedArray[index] = kenanDeck[index];
         }
     }
 }
